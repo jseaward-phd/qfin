fd7d9256251e3c68accb601e46723d6526aa3ec4
J. Seaward <joeseaward@gmail.com>
2025-05-17 16:00:27 -0400

    BiLSTM model construction added.

---
+++ models/BiLSTM.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 17 13:00:30 2025
+
+@author: rakshat
+"""
+
+import tensorflow as tf
+from tensorflow import keras as K
+from tensorflow.keras import layers
+from metrics import sesd, mr, sdr
+
+# build model
+# TODO: config setting/file
+inputs = K.Input(shape=[94,1])
+x = layers.Bidirectional(layers.LSTM(128,activation='tanh', return_sequences=True),merge_mode='sum')(inputs)
+x32 = layers.Bidirectional(layers.LSTM(32,activation='tanh', return_sequences=True),merge_mode='sum')(x)
+x1 = layers.Bidirectional(layers.LSTM(1,activation='linear', return_sequences=True),merge_mode='sum')(x)
+x32 = layers.Bidirectional(layers.LSTM(1,activation='linear', return_sequences=True),merge_mode='sum')(x32)
+output = layers.Add()([x1,x32])
+
+model = K.Model(inputs,output)
+model.summary()
+model.compile(
+    loss = tf.losses.mse,
+    optimizer = K.optimizers.Adam(),
+    metrics= ['mse',sesd, mr, sdr],
+    )
+
---
+++ models/metrics.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 17 14:08:29 2025
+
+@author: rakshat
+
+Metrics from http://arxiv.org/abs/2202.00599 not found in Keras
+"""
+
+from tensorflow.keras import backend as K
+
+def sesd(y_true,y_pred):
+    sq = (y_true-y_pred)**2
+    return K.std(sq)
+
+def mr(y_true,y_pred):
+    return K.mean(y_pred) / K.mean(y_true)
+
+def sdr(y_true,y_pred):
+    return K.std(y_pred/y_true)
